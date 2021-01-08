22cb7e08c7ce9105e5f0e2d51e3ed9ef65e80e89
Yaroslav Holota <korova2321@gmail.com>
2021-01-08 11:27:07 +0200

    Added hangfire actions

---
+++ SchedulerTelegramBot/Hangfire/HangfireActions.cs
@@ -0,0 +1,24 @@
+﻿using SchedulerTelegramBot.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace WebAPI.Hangfire
+{
+    public class HangfireActions
+    {
+        private readonly ITelegramClientAdapter _client;
+
+        public HangfireActions(ITelegramClientAdapter client)
+        {
+            this._client = client;
+        }
+
+        public async Task SendAlertMessage(string Message, string ChatId)
+        {
+            await _client.SendTextMessageAsync(new ChatId(ChatId), Message);
+        }
+    }
+}
