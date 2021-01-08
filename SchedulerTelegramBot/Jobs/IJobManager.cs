a4f12b817fab32f4c33cc710c50b92adb71d6a15
Yaroslav Holota <korova2321@gmail.com>
2021-01-08 11:40:44 +0200

    Added job manager, that setups jobs

---
+++ SchedulerTelegramBot/Jobs/IJobManager.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace WebAPI.Jobs
+{
+    public interface IJobManager
+    {
+        Task SetupJobsForChat(ScheduleModel model, ChatId chat);
+    }
+}
---
+++ SchedulerTelegramBot/Jobs/JobManager.cs
@@ -0,0 +1,49 @@
+﻿using Hangfire;
+using Infrastructure.DTOs;
+using Infrastructure.Repositories;
+using SchedulerTelegramBot.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using WebAPI.Hangfire;
+
+namespace WebAPI.Jobs
+{
+    public class JobManager : IJobManager
+    {
+        private readonly IScheduleRepo _repo;
+        private readonly ITelegramClientAdapter _client;
+
+        public JobManager(IScheduleRepo repo, ITelegramClientAdapter client)
+        {
+            this._repo = repo;
+            this._client = client;
+        }
+
+        public async Task SetupJobsForChat(ScheduleModel model, ChatId chat)
+        {
+            try
+            {
+                string chatId = chat.Identifier.ToString();
+                await _repo.TryApplyScheduleToChat(model, chatId);
+                int JobCount = 0;
+                foreach (AlertModel alert in model.Alerts)
+                {
+                    RecurringJob.AddOrUpdate<HangfireActions>(GenerateJobId(chatId, JobCount),
+                        x=>x.SendAlertMessage(alert.Message, chatId), alert.Cron);
+                    JobCount++;
+                }
+            }
+            catch (Exception ex)
+            {
+                await _client.SendTextMessageAsync(chat, $"Something went wrong, while adding jobs: {ex.Message}");
+            }
+        }
+        private string GenerateJobId(string chatId, int count)
+        {
+            return $"{chatId}_{count}";
+        }
+    }
+}
