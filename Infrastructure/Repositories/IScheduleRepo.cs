e08c54a7040f790b812fb692bf484d763d5193b7
Yaroslav Holota <korova2321@gmail.com>
2021-01-08 11:29:27 +0200

    Added repositories to work with schedules

---
+++ Infrastructure/Repositories/IScheduleRepo.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public interface IScheduleRepo
+    {
+        Task TryApplyScheduleToChat(ScheduleModel schedule, string ChatId); 
+    }
+}
---
+++ Infrastructure/Repositories/ScheduleRepo.cs
@@ -0,0 +1,40 @@
+﻿using Core;
+using Infrastructure.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class ScheduleRepo : IScheduleRepo
+    {
+        private readonly SchedulesContext _context;
+        private readonly IModelConverter _converter;
+
+        public ScheduleRepo(SchedulesContext context, IModelConverter converter)
+        {
+            this._context = context;
+            this._converter = converter;
+        }
+        public async Task TryApplyScheduleToChat(ScheduleModel schedule, string ChatId)
+        {
+            var chat = await _context.Chats.FirstAsync(chat => chat.ChatId == ChatId);
+            if(chat is null)
+            {
+                throw new Exception("No such chat");
+            }
+            if(chat.Schedule is null)
+            {
+                chat.Schedule = _converter.ConvertScheduleFromDTO(schedule);
+            }
+            else
+            {
+                throw new Exception("This chat already has schedule attached to it");
+            }
+            _context.SaveChanges();
+        }
+    }
+}
