59bf6e9219f58c998d77f671e3db0b9bc1ffbee9
Yaroslav Holota <korova2321@gmail.com>
2021-01-09 10:38:41 +0200

    Added command container

---
+++ SchedulerTelegramBot/Commands/MessageRepliesContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Commands
+{
+    public class MessageRepliesContainer
+    {
+        private readonly List<MessageReplyBase> messageReplies = new List<MessageReplyBase>();
+        public MessageRepliesContainer(StartCommand start)
+        {
+            messageReplies.Add(start);
+        }
+        public IReadOnlyList<MessageReplyBase> GetMessageReplies()
+        {
+            return messageReplies.AsReadOnly();
+        }
+    }
+}
