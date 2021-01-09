31a7de71d79be9eca9350f4a838859e514e7facb
Yaroslav Holota <korova2321@gmail.com>
2021-01-09 12:03:01 +0200

    Created alert repository
    It is used to directly access alerts

---
+++ Infrastructure/Repositories/AlertRepo.cs
@@ -0,0 +1,28 @@
+﻿using Core;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class AlertRepo : IAlertRepo
+    {
+        private readonly SchedulesContext _context;
+
+        public AlertRepo(SchedulesContext context)
+        {
+            this._context = context;
+        }
+        public async Task UpdateJobId(Alert alert, string jobId)
+        {
+            _context.Attach(alert);
+
+            alert.JobId = jobId;
+
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Infrastructure/Repositories/IAlertRepo.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public interface IAlertRepo
+    {
+        Task UpdateJobId(Alert alert, string jobId); 
+    }
+}
