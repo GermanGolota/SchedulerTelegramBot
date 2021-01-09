39995ec3c90c2aa4729af14d5a7ba8bc7f7ab0ca
Yaroslav Holota <korova2321@gmail.com>
2021-01-09 10:29:47 +0200

    Created chat repository
    It would be used to retrieve info about chat

---
+++ Infrastructure/Repositories/ChatRepo.cs
@@ -0,0 +1,29 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.Repositories
+{
+    public class ChatRepo : IChatRepo
+    {
+        private readonly SchedulesContext _context;
+
+        public ChatRepo(SchedulesContext context)
+        {
+            this._context = context;
+        }
+        public string GetAdminIdOfChat(string chatId)
+        {
+            var chat = _context.Chats.First(chat => chat.ChatId == chatId);
+
+            if(chat is null)
+            {
+                throw new Exception("That chat is not yet in the system");
+            }
+
+            return chat.AdminId;
+        }
+    }
+}
---
+++ Infrastructure/Repositories/IChatRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repositories
+{
+    public interface IChatRepo
+    {
+        string GetAdminIdOfChat(string ChatId);
+    }
+}
