64722c8e3400ba078f4a7a0efccfa9b900c66647
Yaroslav Holota <korova2321@gmail.com>
2021-01-10 10:43:49 +0200

    Added exception for chat already containg schedule attached to it

--- Infrastructure/Exceptions/ExceptionMessages.cs
+++ Infrastructure/Exceptions/ExceptionMessages.cs
@@ -6,7 +6,8 @@ namespace Infrastructure.Exceptions
 {
     public static class ExceptionMessages
     {
-        public static string ChatExistsMessage = "This chat is alredy registered";
-        public static string ChatDontExistsMessage = "That chat is not yet in the system";
+        public static string ChatExistsMessage { get;  } = "This chat is alredy registered";
+        public static string ChatDontExistsMessage { get; } = "That chat is not yet in the system";
+        public static string ScheduleAlreadyAttached { get; } = "This chat already has schedule attached to it";
     }
 }
