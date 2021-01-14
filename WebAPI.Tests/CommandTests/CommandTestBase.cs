9cb08b9dcf4dc7a33fdd5ea64f93b1e94fea9e86
Yaroslav Holota <korova2321@gmail.com>
2021-01-14 10:43:07 +0200

    Separated basic command testing into separate base class

---
+++ WebAPI.Tests/CommandTests/CommandTestBase.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using SchedulerTelegramBot.Client;
+using Telegram.Bot.Types;
+
+namespace SchedulerTelegramBot.Tests
+{
+    public class CommandTestBase
+    {
+        protected readonly Mock<ITelegramClientAdapter> _clientMock = new Mock<ITelegramClientAdapter>();
+
+        protected const string TestChatId = "56675";
+
+        protected const string UserId = "12345";
+        protected void SetupMessageSendingMock()
+        {
+            _clientMock.Setup(x => x.SendTextMessageAsync(It.IsAny<ChatId>(), It.IsAny<string>()));
+            _clientMock.Setup(x => x.SendStickerAsync(It.IsAny<ChatId>(), It.IsAny<string>()));
+        }
+
+    }
+}
--- WebAPI.Tests/CommandTests/Matchers/CommandMatcherTestBase.cs
+++ WebAPI.Tests/CommandTests/Matchers/CommandMatcherTestBase.cs
@@ -1,30 +1,10 @@
-﻿using Moq;
-using SchedulerTelegramBot.Client;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Telegram.Bot.Types;
-using WebAPI.Commands;
-using Xunit;
+﻿using Xunit;
 
 namespace SchedulerTelegramBot.Tests
 {
-    
-    public class CommandMatcherTestBase
+
+    public class CommandMatcherTestBase: CommandTestBase
     {
-        protected readonly Mock<ITelegramClientAdapter> _clientMock = new Mock<ITelegramClientAdapter>();
-
-        protected const string TestChatId = "56675";
-
-        protected const string UserId = "12345";
-        protected void SetupMessageSendingMock()
-        {
-            _clientMock.Setup(x => x.SendTextMessageAsync(It.IsAny<ChatId>(), It.IsAny<string>()));
-            _clientMock.Setup(x => x.SendStickerAsync(It.IsAny<ChatId>(), It.IsAny<string>()));
-        }
-
         protected void AssertCommandMatched(bool actual)
         {
             Assert.True(actual);
