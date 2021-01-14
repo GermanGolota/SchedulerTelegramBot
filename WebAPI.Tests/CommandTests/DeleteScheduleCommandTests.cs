1fc68771f24cab8e8a29f0e5f93a4a5cb162ef42
Yaroslav Holota <korova2321@gmail.com>
2021-01-14 10:53:38 +0200

    Created basic tests for delete schedule command

---
+++ WebAPI.Tests/CommandTests/DeleteScheduleCommandTests.cs
@@ -0,0 +1,63 @@
+﻿using Moq;
+using SchedulerTelegramBot.Tests.Mocks;
+using System;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using WebAPI.Commands;
+using WebAPI.Commands.Verifiers;
+using WebAPI.Jobs;
+using Xunit;
+
+namespace SchedulerTelegramBot.Tests
+{
+    public class DeleteScheduleCommandTests:CommandTestBase
+    {
+        private DeleteScheduleCommand _sut;
+        private readonly Mock<IMatcher<DeleteScheduleCommand>> _matcherMock = new Mock<IMatcher<DeleteScheduleCommand>>();
+        private readonly Mock<IJobManager> _jobMock = new Mock<IJobManager>();
+        public DeleteScheduleCommandTests()
+        {
+            _sut = new DeleteScheduleCommand(_matcherMock.Object, _clientMock.Object,
+                _jobMock.Object, new LoggerMock<DeleteScheduleCommand>());
+        }
+        [Fact]
+        public async Task ExecuteCommandIfMatched_ShouldSetupJobs()
+        {
+            //Arrange
+            SetupMessageSendingMock();
+            _jobMock.Setup(x => x.DeleteJobsFromChat(It.IsAny<ChatId>()));
+            _matcherMock.Setup(x => x.IsMatching(It.IsAny<Update>())).ReturnsAsync(true);
+
+
+            Update update = GetUpdate();
+            //Act
+            await _sut.ExecuteCommandIfMatched(update);
+            //Assert
+            AssertJobBeenPerformed();
+        }
+
+        private void AssertJobBeenPerformed()
+        {
+            _jobMock.Verify(x => x.DeleteJobsFromChat(It.IsAny<ChatId>()), Times.Once);
+        }
+
+        private Update GetUpdate()
+        {
+            return new Update
+            {
+                Message = new Message
+                {
+                    Text = "/deleteSchedule",
+                    Chat = new Chat
+                    {
+                        Id = long.Parse(TestChatId)
+                    },
+                    From = new User
+                    {
+                        Id = int.Parse(UserId)
+                    }
+                }
+            };
+        }
+    }
+}
