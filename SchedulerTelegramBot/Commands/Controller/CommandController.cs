d0786ad5dcc72548e2166054c5838ce8f0ffccdf
Yaroslav Holota <korova2321@gmail.com>
2021-01-16 15:11:56 +0200

    Create controller, that would control execution of command

---
+++ SchedulerTelegramBot/Commands/Controller/CommandController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using WebAPI.Commands.Verifiers;
+
+namespace WebAPI.Commands
+{
+    public class CommandController<T> : ICommandController where T : ICommand
+    {
+        private readonly IMatcher<T> _matcher;
+        private readonly T _coommand;
+        public CommandController(IMatcher<T> matcher, T coommand)
+        {
+            this._matcher = matcher;
+            this._coommand = coommand;
+        }
+        public async Task<CommandMatchResult> CheckCommand(Update update)
+        {
+            if (await _matcher.IsMatching(update))
+            {
+                await _coommand.Execute(update);
+                return CommandMatchResult.Matching;
+            }
+            else
+            {
+                return CommandMatchResult.NotMatching;
+            }
+        }
+    }
+}
---
+++ SchedulerTelegramBot/Commands/Controller/ICommandController.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace WebAPI.Commands
+{
+    //needed to avoid using generic type during DI
+    public interface ICommandController
+    {
+        Task<CommandMatchResult> CheckCommand(Update update);
+    }
+}
