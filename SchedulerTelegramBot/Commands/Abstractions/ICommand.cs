1028d010d20b7eca4577b594d42af010be4623de
Yaroslav Holota <korova2321@gmail.com>
2021-01-16 15:11:22 +0200

    Added new interface to command

---
+++ SchedulerTelegramBot/Commands/Abstractions/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace WebAPI.Commands
+{
+    public interface ICommand
+    {
+        Task Execute(Update update);
+    }
+}
