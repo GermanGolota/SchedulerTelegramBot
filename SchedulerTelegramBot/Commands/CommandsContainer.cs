e0ebf9d404c55c2cbd826c506f098267ac4a9745
Yaroslav Holota <korova2321@gmail.com>
2021-01-16 15:13:40 +0200

    Changed message container to find all commands using reflection

---
+++ SchedulerTelegramBot/Commands/CommandsContainer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace WebAPI.Commands
+{
+    public class CommandsContainer
+    {
+        private readonly List<ICommand> messageReplies = new List<ICommand>();
+        public CommandsContainer(IServiceProvider provider)
+        {
+            IEnumerable<Type> commandTypes = GetAllChildTypes(typeof(ICommand));
+            using (var scope = provider.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                foreach (var commandType in commandTypes)
+                {
+                    ICommand command = (ICommand)serviceProvider.GetRequiredService(commandType);
+                    messageReplies.Add(command);
+                }
+            }
+        }
+        public IReadOnlyList<ICommand> GetMessageReplies()
+        {
+            return messageReplies.AsReadOnly();
+        }
+        private IEnumerable<Type> GetAllChildTypes(Type parentClass)
+        {
+            return Assembly.GetExecutingAssembly().GetTypes()
+                .Where(x=>!x.IsInterface&&x.GetInterfaces().Contains(parentClass));
+        }
+    }
+}
