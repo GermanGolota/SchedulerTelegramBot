ac8152c8d5fd7635bf89fc611ad342d1fc7a9cbf
Yaroslav Holota <korova2321@gmail.com>
2021-01-16 19:04:21 +0200

    Created reflection "helpers" as extension methods

---
+++ SchedulerTelegramBot/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using WebAPI.Commands;
+using WebAPI.Commands.Verifiers;
+
+namespace WebAPI.Extensions
+{
+    public static class ReflectionExtensions
+    {
+        public static IEnumerable<Type> GetTypesThatImplement(this Assembly assembly, Type interfaceType)
+        { 
+            List<Type> commands = assembly.GetTypes().Where(x => x.GetInterfaces().Contains(interfaceType)).ToList();
+            return commands;
+        }
+        public static Type GetIMatcherFor(this Assembly assembly, Type command)
+        {
+            Type output = typeof(IMatcher<>);
+            output = output.MakeGenericType(command);
+            return output;
+        }
+        public static Type GetMatcherImplementationFor(this Assembly assembly, Type command)
+        {
+            Type output = assembly.GetTypes()
+               .Where(x => x.GetInterfaces()
+                   .Where(i => i.Name.Equals(typeof(IMatcher<>).Name) && i.GetGenericArguments().Contains(command)).Any())
+               .FirstOrDefault();
+            return output ;
+        }
+        private static IEnumerable<Type> InheritsFrom(this IEnumerable<Type> types, Type interfaceType)
+        {
+            return types.Where(x => x.GetInterfaces().Contains(interfaceType));
+        }
+    }
+}
--- WebAPI.Tests/ReflectionTests.cs
+++ WebAPI.Tests/ReflectionTests.cs
@@ -7,30 +7,12 @@ using System.Threading.Tasks;
 using WebAPI.Commands;
 using WebAPI.Commands.Verifiers;
 using Xunit;
+using WebAPI.Extensions;
 
 namespace SchedulerTelegramBot.Tests
 {
     public class ReflectionTests
     {
-        [Fact]
-        private void ShouldReturnAppropriateMatcher()
-        {
-            var expected = typeof(StartCommandMatcher);
-
-            var actual =  GetMatcherType(typeof(StartCommand));
-
-            Assert.Equal(expected.Name, actual.Name);
-        }
-        private Type GetMatcherType(Type commandType)
-        {
-            Assembly assembly = typeof(StandardMessages).Assembly;
-            Type output = assembly.GetTypes()
-                .Where(x => x.GetInterfaces()
-                .Where(i => i.Name.Equals(typeof(IMatcher<>).Name) && i.GetGenericArguments().Contains(commandType)).Any())
-                .FirstOrDefault();
-
-            return output ?? throw new ArgumentException("No such command", nameof(commandType));
-        }
         [Fact]
         public void TypeShouldContainAssignedGenericType()
         {
@@ -39,6 +21,52 @@ namespace SchedulerTelegramBot.Tests
             controllerType = controllerType.MakeGenericType(commandType);
 
             Assert.Contains(commandType, controllerType.GetGenericArguments());
+        }
+        [Fact]
+        public void ShouldGetAllCommands()
+        {
+            Assembly assembly = GetWEBAPIAssembly();
+            Type command = typeof(ICommand);
+            List<Type> commands = assembly.GetTypesThatImplement(command).ToList();
+
+            Assert.Equal(4, commands.Count);
+            Assert.Contains(typeof(StartCommand), commands);
+            Assert.Contains(typeof(SetupCommand), commands);
+            Assert.Contains(typeof(DeleteScheduleCommand), commands);
+            Assert.Contains(typeof(DeleteChatCommand), commands);
+        }
+        [Fact]
+        public void ShouldGetMatcher()
+        {
+            Assembly assembly = GetWEBAPIAssembly();
+
+            Type command = typeof(StartCommand);
+            Type expected = typeof(IMatcher<StartCommand>);
+
+            Type actual = assembly.GetIMatcherFor(command);
+
+            Assert.Equal(expected.Name, actual.Name);
+            Type expectedGeneric = expected.GetGenericArguments().FirstOrDefault();
+            Type actualGeneric = actual.GetGenericArguments().FirstOrDefault();
+
+            Assert.Equal(actualGeneric, expectedGeneric);
+
+        }
+        [Fact]
+        public void ShouldGetMatcherImplementation()
+        {
+            Assembly assembly = GetWEBAPIAssembly();
+            Type command = typeof(StartCommand);
+            Type expected = typeof(StartCommandMatcher);
+
+            Type actual = assembly.GetMatcherImplementationFor(command);
+
+            Assert.Equal(expected.Name, actual.Name);
+        }
+        public Assembly GetWEBAPIAssembly()
+        {
+            Assembly assembly = typeof(StandardMessages).Assembly;
+            return assembly;
         }
     }
 }
