d90909b81cd4143ae6802365ba24b0370e6569a5
Yaroslav Holota <korova2321@gmail.com>
2021-01-17 17:48:02 +0200

    Added basic functional of new command

---
+++ WebAPI/Commands/Executors/Implementations/CreateScheduleCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using WebAPI.Client;
+
+namespace WebAPI.Commands
+{
+    public class CreateScheduleCommand : ICommand
+    {
+        private readonly ITelegramClientAdapter _client;
+
+        public CreateScheduleCommand(ITelegramClientAdapter client)
+        {
+            this._client = client;
+        }
+        public async Task Execute(Update update)
+        {
+            var chatId = update.Message.Chat.Id;
+            await _client.SendTextMessageAsync(chatId, "This feature is not yet implemented");
+        }
+    }
+}
---
+++ WebAPI/Commands/Matchers/Implementations/CreateScheduleCommandMatcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using WebAPI.Commands.Verifiers;
+
+namespace WebAPI.Commands
+{
+    public class CreateScheduleCommandMatcher : RequestMatcherBase<CreateScheduleCommand>
+    {
+        private string commandName = "createSchedule";
+        public override async Task<bool> IsMatching(Update update)
+        {
+            if (UpdateIsCommand(update))
+            {
+                string messageText = update.Message.Text ?? "";
+
+                if (FirstWordMatchesCommandName(messageText, commandName))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
