18a3001f61120311c0dcdc1f151ab5c3eb636d11
Yaroslav Holota <korova2321@gmail.com>
2021-01-18 13:02:05 +0200

    Moved command names into the strongely typed class

---
+++ WebAPI/Commands/CommandNames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Commands
+{
+    public static class CommandNames
+    {
+        public static string Setup { get; } = "setup";
+        public static string DeleteSchedule { get; } = "deleteSchedule";
+        public static string DeleteChat { get; } = "deleteChat";
+        public static string Start { get; } = "start";
+        public static string CreateSchedule { get; } = "createSchedule";
+    }
+}
