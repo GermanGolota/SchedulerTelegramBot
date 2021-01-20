1922446e0dd6d5fb08a121a9e7a84293b9394b03
Yaroslav Holota <korova2321@gmail.com>
2021-01-20 16:59:03 +0200

    Created corresponding matcher

---
+++ WebAPI/Commands/Matchers/Implementations/AddAlertsCommandMatcher.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Client;
+
+namespace WebAPI.Commands.Verifiers
+{
+    public class AddAlertsCommandMatcher:FileAdminCommandMatcherBehaviour<AddAlertCommand>
+    {
+        public AddAlertsCommandMatcher(ITelegramClientAdapter client, IChatRepo repo)
+            :base(client, repo, CommandNames.AddAlerts)
+        {
+
+        }
+    }
+}
