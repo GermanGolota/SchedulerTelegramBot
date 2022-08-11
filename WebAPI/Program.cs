d7af3591b88502be4d9074aaaf5381eff19377de
korov <korova2321@gmail.com>
2022-08-11 04:25:46 +0300

    Move long pulling into separate service

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -49,6 +49,7 @@ namespace WebAPI
                     webBuilder.UseStartup<Startup>();
                 });
         }
+
         private static async Task InitializeTelegramClient(IHost host)
         {
             using (var scope = host.Services.CreateScope())
@@ -58,67 +59,10 @@ namespace WebAPI
                 var config = provider.GetService<IConfiguration>();
                 string useLongPulling = config["UseLongPulling"] ?? "false";
                 await client.BootUpClient();
-                if (useLongPulling.Equals("true"))
-                {
-                    await UseLongPullingInNewThread(host);
-                }
-                else
+                if (useLongPulling.Equals("true") == false)
                 {
                     string webhook = config["Webhook"];
                     await client.SetupWebhook(webhook);
-                }
-            }
-        }
-        private static async Task UseLongPullingInNewThread(IHost host)
-        {
-            Task.Run(() => GetUpdatesManually(host));
-        }
-        private static async Task GetUpdatesManually(IHost host)
-        {
-            using (var scope = host.Services.CreateScope())
-            {
-                var provider = scope.ServiceProvider;
-                CommandsContainer commandsContainer = provider.GetService<CommandsContainer>();
-                var controller = new MessageController(commandsContainer, provider);
-                var config = provider.GetService<IConfiguration>();
-                string token = config.GetValue<string>("Token");
-                TelegramBotClient client = new TelegramBotClient(token);
-                await client.DeleteWebhookAsync();
-
-                var initialUpdates = await client.GetUpdatesAsync();
-
-                var lastUpdate = initialUpdates.LastOrDefault();
-
-                int lastUpdateId = lastUpdate?.Id ?? 0;
-
-                var logger = provider.GetRequiredService<ILogger<Program>>();
-
-                while (true)
-                {
-                    try
-                    {
-                        var updates = await client.GetUpdatesAsync(offset: lastUpdateId + 1);
-
-                        foreach (var update in updates)
-                        {
-                            try
-                            {
-                                await controller.Update(update);
-                                logger.LogInformation("Send message in responce to update");
-                            }
-                            catch (Exception exc)
-                            {
-                                logger.LogError(exc, "Something went wrong while sending reply");
-                            }
-                            lastUpdateId = update.Id;
-                        }
-
-                        await Task.Delay(1000);
-                    }
-                    catch (Exception exc)
-                    {
-                        logger.LogError(exc, "Something went wrong while receiving updates");
-                    }
                 }
             }
         }
---
+++ WebAPI/Services/LongPullingTelegramService.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SchedulerTelegramBot.Controllers;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using WebAPI.Commands;
+
+namespace WebAPI.Services
+{
+    public class LongPullingTelegramService : BackgroundService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public LongPullingTelegramService(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var provider = scope.ServiceProvider;
+                CommandsContainer commandsContainer = provider.GetService<CommandsContainer>();
+                var controller = new MessageController(commandsContainer, provider);
+                var config = provider.GetService<IConfiguration>();
+                string token = config.GetValue<string>("Token");
+                TelegramBotClient client = new(token);
+                await client.DeleteWebhookAsync();
+
+                var initialUpdates = await client.GetUpdatesAsync(cancellationToken: stoppingToken);
+
+                var lastUpdate = initialUpdates.LastOrDefault();
+
+                int lastUpdateId = lastUpdate?.Id ?? 0;
+
+                var logger = provider.GetRequiredService<ILogger<LongPullingTelegramService>>();
+
+                while (stoppingToken.IsCancellationRequested == false)
+                {
+                    try
+                    {
+                        var updates = await client.GetUpdatesAsync(offset: lastUpdateId + 1, cancellationToken: stoppingToken);
+
+                        foreach (var update in updates)
+                        {
+                            try
+                            {
+                                await controller.Update(update);
+                                logger.LogInformation("Send message in responce to update");
+                            }
+                            catch (Exception exc)
+                            {
+                                logger.LogError(exc, "Something went wrong while sending reply");
+                            }
+                            lastUpdateId = update.Id;
+                        }
+
+                        await Task.Delay(1000, stoppingToken);
+                    }
+                    catch (Exception exc)
+                    {
+                        logger.LogError(exc, "Something went wrong while receiving updates");
+                    }
+                }
+            }
+        }
+    }
+}
